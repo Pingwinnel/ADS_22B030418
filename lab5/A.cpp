051cc457b154b4cd9c53fbfc088075c119c9efca
Pingwinnel <erasil123gns@gmail.com>
2023-10-19 10:31:57 +0600

    lab5

--- lab5/A.cpp
+++ lab5/A.cpp
@@ -1,8 +1,67 @@
 #include <iostream>
+#define ll long long int
 using namespace std;
+//run id:849
 
+ll arr[100000];
+ll heapSize;
 
-//run id :
-int main(){
-   
+int parent(int i){
+    return (i / 2);
+}
+int leftCh(int i){
+    return (2 * i);
+}
+int rightCh(int i){
+    return ((2 * i) + 1);
+}
+void heapifyUp(ll arr[],ll i){
+    while (i > 1 && arr[parent(i)] > arr[i]) {
+        swap(arr[parent(i)], arr[i]);
+         i = parent(i);
+    }
+}
+void heapifyDown(ll arr[],ll i){
+    int maxIndex = i;
+    int l = leftCh(i);
+    if (l <= heapSize && arr[l] < arr[maxIndex]) {
+        maxIndex = l;
+    }
+    int r = rightCh(i);
+    if (r <= heapSize && arr[r] < arr[maxIndex]) {
+        maxIndex = r;
+    }
+    if (i != maxIndex) {
+        swap(arr[i], arr[maxIndex]);
+        heapifyDown(arr,maxIndex);
+    }
+}
+void heapDelete(ll index){
+    swap(arr[heapSize],arr[index]);
+    heapSize--;
+    heapifyDown(arr,index);
+
+}
+int main() {
+    ll size,x,count=0;
+    cin>>size;
+    cin>>x;
+    heapSize++;
+    arr[heapSize]=x;
+    for(ll i=1;i<size;i++){
+        cin>>x;
+        heapSize++;
+        arr[heapSize]=x;
+        heapifyUp(arr,heapSize);
+    }
+    while(heapSize>1){
+       heapDelete(1);
+       heapDelete(1);
+       arr[heapSize+1]=arr[heapSize+1]+arr[heapSize+2];
+       heapSize++;
+       count+=arr[heapSize];
+       heapifyUp(arr,heapSize);
+    }
+    cout<<count;
+    return 0;
 }
--- lab5/B.cpp
+++ lab5/B.cpp
@@ -1,8 +1,69 @@
 #include <iostream>
+#define ll long long int
 using namespace std;
+//run id:1272
+ll arr[1000];
+ll heapSize;
 
+int parent(int i){
+    return (i / 2);
+}
+int leftChild(int i){
+    return (2 * i);
+}
+int rightChild(int i){
+    return ((2 * i) + 1);
+}
+void heapify_up(ll arr[],ll i){
+    while (i > 1 && arr[parent(i)] < arr[i]) {
+        swap(arr[parent(i)], arr[i]);
+         i = parent(i);
+    }
+}
+void heapifydown(ll arr[],ll i){
+    int maxIndex = i;
+    int l = leftChild(i);
+    if (l <= heapSize && arr[l] > arr[maxIndex]) {
+        maxIndex = l;
+    }
+    int r = rightChild(i);
+    if (r <= heapSize && arr[r] > arr[maxIndex]) {
+        maxIndex = r;
+    }
+    if (i != maxIndex) {
+        swap(arr[i], arr[maxIndex]);
+        heapifydown(arr,maxIndex);
+    }
+}
+void heapDelete(ll index){
+    swap(arr[heapSize],arr[index]);
+    heapSize--;
+    heapifydown(arr,index);
 
-//run id :
-int main(){
-   
+}
+int main() {
+    ll size,x,count=0;
+    cin>>size;
+    cin>>x;
+    heapSize++;
+    arr[heapSize]=x;
+    for(ll i=1;i<size;i++){
+        cin>>x;
+        heapSize++;
+        arr[heapSize]=x;
+        heapify_up(arr,heapSize);
+    }
+    while(heapSize>1){
+        heapDelete(1);
+        heapDelete(1);
+        if(arr[heapSize+1]!=arr[heapSize+2]){
+            arr[heapSize+1]=abs(arr[heapSize+1]-arr[heapSize+2]);
+            heapSize++;
+        }
+    }  
+    if(heapSize!=0){
+        cout<<arr[heapSize];
+    }else{
+        cout<<0;
+    }
 }
